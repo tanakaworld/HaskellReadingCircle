dc45cda78b920077e42d4c9ef3fd5ad472f6ce9a
yutaro <y.tanaka.kcbbf@gmail.com>
2014-02-23 22:32:49 +0900

    よく使う型を用いた宣言を追加

--- memo20140224.hs
+++ memo20140224.hs
@@ -8,6 +8,7 @@
 
 -- :t 'a'
 
+
 -- リスト内包表記の型宣言
 
 moveNonUppercase :: [Char] -> [Char]
@@ -15,3 +16,20 @@ moveNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
 
 addThree :: Int -> Int -> Int -> Int
 addThree x y z = x + y + z
+
+
+-- 一般的なHaskellの型
+
+-- Integer
+factorial :: Integer -> Integer
+factorial n = product [1..n]
+
+-- Float
+circumference :: Float -> Float
+circumference r = 2 * pi * r
+
+-- Double
+circumference' :: Double -> Double
+circumference' r = 2 * pi * r
+
+
