b7cd8c504d1002392630f5d3cc5db637a4b6256b
yutaro <y.tanaka.kcbbf@gmail.com>
2014-02-24 09:29:27 +0900

    様々な型クラスについて追加

--- memo20140224.hs
+++ memo20140224.hs
@@ -60,3 +60,147 @@ circumference' r = 2 * pi * r
 	fst i.e.
 	「タプルを引数に取り、その一つ目と同じ型の値を返す」
 -}
+
+
+-- --------------------
+-- 2.4 型クラス初級講座
+-- --------------------
+
+
+-- Eq型クラス (等値性をテストできる型)
+
+-- > :t (==)
+-- > (==) :: Eq a => a -> a -> Bool
+{-
+	(==) i.e.
+	「等値性関数は、同じ型の任意の二つの引数を取り、Boolを返す。」
+	「引数の二つの値の型は、Eqクラスのインスタンスで無ければならない」
+-}
+
+-- > 5 == 5
+-- > 5 /= 5
+-- > 'a' == 'a'
+-- > "Ho Ho" == "Ho Ho"\
+-- > 3.456 == 3.456
+
+
+-- Ord型クラス（何らかの順序を付けられる型）
+
+-- > :t (>)
+-- > (>) :: Ord a => a -> a -> Bool
+
+-- > "Hogehogehoge" < "Hugahuga"
+-- > "Hogehogehoge" `compare` "Hugahuga"
+-- > 5 >= 2
+-- > 5 `compare` 2
+
+{-
+	LT Less Than
+	GT Greater Than
+	EQ EQual to
+-}
+
+
+-- Show型クラス（ある値を文字列として表現）
+
+-- > Prelude> :t (show)
+-- > (show) :: Show a => a -> String
+
+-- > show 3
+-- > show 5.334
+-- > show True
+
+
+-- Read型クラス（Showの対となるクラス。文字列をRead型のインスタンス値を返す）
+
+-- > :t (read)
+-- > (read) :: Read a => String -> a
+
+-- > read "True" || False
+-- > read "8.2" + 3.8
+-- > read "5" - 2
+-- > read "[1,2,3,4]" ++ [3]
+-- > read "4"
+
+
+-- 型注釈
+-- > read "4" :: Int
+-- > read "4" :: Float
+-- > (read "4" :: Float) * 4
+-- > read "[1,2,3,4]" :: [Int]
+-- > read "(3, 'a')" :: (Int, Char)
+
+{-
+	静的型付け言語なので、コードをコンパイルする前に、全ての型がわかってる必要がある
+-}
+
+-- > [read "True", False]
+-- > [read "True", False, True, False]
+-- > (read "True", False, True, False)
+
+
+-- Enum型クラス（要素の値を列挙できる型）
+-- > ['a' .. 'e']
+-- > [LT .. GT]
+-- > [3 .. 5]
+-- > succ 'B'
+-- > pred 'B'
+
+
+-- Bounded型クラス
+
+-- > :t minBound
+-- > minBound :: Bounded a => a
+
+-- > minBound :: Int
+-- > maxBound :: Char
+-- > maxBound :: Bool
+-- > minBound :: Bool
+
+-- > maxBound :: (Bool, Int, Char)
+
+
+-- Num型クラス
+-- > :t 20
+-- > 20 :: Num a => a
+
+-- > 20 :: Int
+-- > 20 :: Intger
+-- > 20 :: Float
+-- > 20 :: Double
+
+
+-- > :t (*)
+-- > (*) :: Num a => a -> a -> a
+
+{-
+	「*」が二つの数を受け取り、一つの数を返す
+	これら三つの数はすべて同じ型である
+-}
+
+
+-- > (5 :: Int) * (6 :: Integer)
+-- > 5 * (6 :: Integer)
+
+
+-- Floating型クラス
+
+-- > :t sin
+-- > sin :: Floating a => a -> a
+-- > :t cos
+-- > cos :: Floating a => a -> a
+-- > :t sqrt
+-- > sqrt :: Floating a => a -> a
+
+
+-- Integral型クラス
+
+-- > :t fromIntegral
+-- > fromIntegral :: (Integral a, Num b) => a -> b
+
+{-
+	Num : 実数を含む全ての数を含む
+	Integral : 整数（全体）のみ含む
+-}
+
+-- > fromIntegral (length [1,2,3,4]) + 3.2
